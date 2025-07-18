ebe6b878ea6b6e0c8df889ae32b60567215ec20b
BingoSource <Saraicorreo2023@gmail.com>
2025-07-18 02:35:16 -0400

    Delete Jesuspagina directory

--- Jesuspagina/index.js
+++
@@ -1,9 +0,0 @@
-function continueToMain() {
-    const username = document.getElementById('usernameInput').value.trim();
-    if (username) {
-        localStorage.setItem('username', username);
-        window.location.href = "./P/P.html";
-    } else {
-        alert("Por favor, ingrese un nombre de usuario válido.");
-    }
-}
